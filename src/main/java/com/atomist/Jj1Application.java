eeb774a60e9ef1523f0ad58d327a16350c0e8776
Rod Johnson <johnsonroda@gmail.com>
2018-05-23 16:45:14 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/Jj1Application.java
+++ src/main/java/com/atomist/Jj1Application.java
@@ -1,3 +1,5 @@
+// Finite games are played within boundaries. Infinite games play with boundaries.
+
 // I believe in you. You can do the thing!
 
 // It’s only a thought, and a thought can be changed.
